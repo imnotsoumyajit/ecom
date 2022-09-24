7d9cc7c1de8de27694266933c457e6a90ebdf6b7
Soumyajit <soumyajitm784@gmail.com>
2022-09-24 13:12:58 +0530

    Few dummy products added

---
+++ components/ProductItem.js
@@ -0,0 +1,28 @@
+import React from "react";
+import Link from "next/link";
+
+export default function ProductItem({ product }) {
+  return (
+    <div className="card">
+      <Link href={`product/${product.slug}`}>
+        <a>
+          <img
+            src={product.image}
+            alt={product.name}
+            className="rounded shadow"
+          />
+        </a>
+      </Link>
+      <div className="flex flex-col items-center justify-center p-5">
+        <Link href={`/products/${product.slug}`}>
+          <a>
+            <h5 className="text-lg">{product.name}</h5>
+          </a>
+        </Link>
+        <p className="mb-2">{product.brand}</p>
+        <p>₹{product.price}</p>
+        <button className="primary-button " type="button">Add to cart</button>
+      </div>
+    </div>
+  );
+}
