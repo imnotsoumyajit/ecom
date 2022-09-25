f63231360ccfbfb26ba165ea51568b52afbea6ac
Soumyajit <soumyajitm784@gmail.com>
2022-09-25 09:47:06 +0530

    Product details ( basic one) added

--- components/Layout.js
+++ components/Layout.js
@@ -30,9 +30,7 @@ export default function Layout({ title, children }) {
           </nav>
         </header>
 
-        <main className="container m-auto mt-4 px-4">
-          {children}
-        </main>
+        <main className="container m-auto mt-4 px-4">{children}</main>
 
         <footer className="flex h-10 justify-center items-center shadow-inner">
           <p>Copyright © 2022 Paradise Collectables</p>
