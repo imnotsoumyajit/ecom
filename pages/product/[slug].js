f77a3a31351b03425e065fbc91884b2e05c63ace
Soumyajit <soumyajitm784@gmail.com>
2022-09-26 13:42:26 +0530

    Working add cart logic implemented

--- pages/product/[slug].js
+++ pages/product/[slug].js
@@ -1,11 +1,14 @@
 import Image from "next/image";
 import Link from "next/link";
 import { useRouter } from "next/router";
-import React from "react";
+import React, { useContext } from "react";
 import Layout from "../../components/Layout";
 import data from "../../utils/data";
+import { Store } from "../../utils/Store";
 
 export default function ProductScreen() {
+  const { state, dispatch } = useContext(Store);
+
   const { query } = useRouter();
   const { slug } = query;
   const product = data.products.find((x) => x.slug === slug);
@@ -13,10 +16,22 @@ export default function ProductScreen() {
     return <div>Product Not Found</div>;
   }
 
+  const addToCartHandler = () => {
+    const existItem = state.cart.cartItems.find((x) => x.slug === product.slug);
+    const quantity = existItem ? existItem.quantity + 1 : 1;
+
+    if (product.countInStock < quantity) {
+      alert("Sorry. Product is out of stock");
+      return;
+    }
+
+    dispatch({ type: "CART_ADD_ITEM", payload: { ...product, quantity } });
+  };
+
   return (
     <Layout title={product.name}>
       <div className="py-2">
-        <Link href="/">Back to Products</Link>
+        <Link href="/">back to products</Link>
       </div>
       <div className="grid md:grid-cols-4 md:gap-3">
         <div className="md:col-span-2">
@@ -26,7 +41,7 @@ export default function ProductScreen() {
             width={640}
             height={640}
             layout="responsive"
-          />
+          ></Image>
         </div>
         <div>
           <ul>
@@ -36,7 +51,7 @@ export default function ProductScreen() {
             <li>Category: {product.category}</li>
             <li>Brand: {product.brand}</li>
             <li>
-              {product.rating} from {product.numReviews} reviews
+              {product.rating} of {product.numReviews} reviews
             </li>
             <li>Description: {product.description}</li>
           </ul>
@@ -45,15 +60,20 @@ export default function ProductScreen() {
           <div className="card p-5">
             <div className="mb-2 flex justify-between">
               <div>Price</div>
-              <div>₹{product.price}</div>
+              <div>${product.price}</div>
             </div>
             <div className="mb-2 flex justify-between">
               <div>Status</div>
-              <div>{product.countInStock > 0 ? "In Stock" : "Unavailable"}</div>
+              <div>{product.countInStock > 0 ? "In stock" : "Unavailable"}</div>
             </div>
-            <button className="primary-button w-full">Add to cart</button>
+            {/* <button className="primary-button w-full">Add to cart</button> */}
+            <button
+              className="primary-button w-full"
+              onClick={addToCartHandler}
+            >
+              Add to cart
+            </button>
           </div>
-
         </div>
       </div>
     </Layout>
