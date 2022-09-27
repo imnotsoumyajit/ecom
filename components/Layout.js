783ae95fb0c9718d3fd29d138f0328aa7180eed6
Soumyajit <soumyajitm784@gmail.com>
2022-09-27 19:58:47 +0530

    Cookie functionality added

--- components/Layout.js
+++ components/Layout.js
@@ -1,48 +1,52 @@
-// @@ -1,8 +1,11 @@
 import Head from "next/head";
 import Link from "next/link";
 // import React from 'react';
-import React, { useContext } from "react";
+import React, { useContext, useEffect, useState } from "react";
 import { Store } from "../utils/Store";
 
 export default function Layout({ title, children }) {
   const { state } = useContext(Store);
   const { cart } = state;
+  const [cartItemsCount, setCartItemsCount] = useState(0);
+  useEffect(() => {
+    setCartItemsCount(cart.cartItems.reduce((a, c) => a + c.quantity, 0));
+  }, [cart.cartItems]);
+  // the UseEffect only renders on the client side
   return (
     <>
       <Head>
         <title>
           {title ? title + " - Paradise Collectables" : "Paradise Collectables"}
         </title>
-        <meta name="description" content="Ecommerce Website" />
-        <link rel="icon" href="/favicon.ico" />
+        <meta name='description' content='Ecommerce Website' />
+        <link rel='icon' href='/favicon.ico' />
       </Head>
-      <div className="flex min-h-screen flex-col justify-between ">
+      <div className='flex min-h-screen flex-col justify-between '>
         <header>
-          <nav className="flex h-12 items-center px-4 justify-between shadow-md">
-            <Link href="/">
-              <a className="text-lg font-bold">Paradise Collectables</a>
+          <nav className='flex h-12 items-center px-4 justify-between shadow-md'>
+            <Link href='/'>
+              <a className='text-lg font-bold'>Paradise Collectables</a>
             </Link>
             <div>
-              <Link href="/cart">
+              <Link href='/cart'>
                 {/* <a className="p-2">Cart</a> */}
-                <a className="p-2">
+                <a className='p-2'>
                   Cart
-                  {cart.cartItems.length > 0 && (
-                    <span className="ml-1 rounded-full bg-red-600 px-2 py-1 text-xs font-bold text-white">
-                      {cart.cartItems.reduce((a, c) => a + c.quantity, 0)}
+                  {cartItemsCount > 0 && (
+                    <span className='ml-1 rounded-full bg-red-600 px-2 py-1 text-xs font-bold text-white'>
+                      {cartItemsCount}
                     </span>
                   )}
                 </a>
               </Link>
-              <Link href="/login">
-                <a className="p-2">Login</a>
+              <Link href='/login'>
+                <a className='p-2'>Login</a>
               </Link>
             </div>
           </nav>
         </header>
-        <main className="container m-auto mt-4 px-4">{children}</main>
-        <footer className="flex h-10 justify-center items-center shadow-inner">
+        <main className='container m-auto mt-4 px-4'>{children}</main>
+        <footer className='flex h-10 justify-center items-center shadow-inner'>
           <p>Copyright © 2022 Paradise Collectables</p>
         </footer>
       </div>
