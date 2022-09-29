8181b69d862a3ea949af579d698612e9ad0eb46a
Soumyajit <soumyajitm784@gmail.com>
2022-09-29 20:42:04 +0530

    Authentication added(Login functionality)

--- components/Layout.js
+++ components/Layout.js
@@ -1,11 +1,16 @@
+import { useSession } from "next-auth/react";
 import Head from "next/head";
 import Link from "next/link";
 // import React from 'react';
 import React, { useContext, useEffect, useState } from "react";
+import { ToastContainer } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
 import { Store } from "../utils/Store";
 
 export default function Layout({ title, children }) {
+  const { status, data: session } = useSession();
   const { state } = useContext(Store);
+
   const { cart } = state;
   const [cartItemsCount, setCartItemsCount] = useState(0);
   useEffect(() => {
@@ -21,6 +26,8 @@ export default function Layout({ title, children }) {
         <meta name='description' content='Ecommerce Website' />
         <link rel='icon' href='/favicon.ico' />
       </Head>
+      <ToastContainer position='bottom-center' limit={1} />
+
       <div className='flex min-h-screen flex-col justify-between '>
         <header>
           <nav className='flex h-12 items-center px-4 justify-between shadow-md'>
@@ -39,9 +46,16 @@ export default function Layout({ title, children }) {
                   )}
                 </a>
               </Link>
-              <Link href='/login'>
-                <a className='p-2'>Login</a>
-              </Link>
+
+              {status === "loading" ? (
+                "Loading"
+              ) : session?.user ? (
+                session.user.name
+              ) : (
+                <Link href='/login'>
+                  <a className='p-2'>Login</a>
+                </Link>
+              )}
             </div>
           </nav>
         </header>
