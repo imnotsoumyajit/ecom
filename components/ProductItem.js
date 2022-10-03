a6855d3253dde196eba7fe86d5e5cdaa33bb68dd
somo07 <soumyajitm784@gmail.com>
2022-10-03 11:38:26 +0530

    integrate products from db and a few toast message

--- components/ProductItem.js
+++ components/ProductItem.js
@@ -1,27 +1,33 @@
 import React from "react";
 import Link from "next/link";
 
-export default function ProductItem({ product }) {
+export default function ProductItem({ product, addToCartHandler }) {
   return (
-    <div className="card">
+    <div className='card'>
       <Link href={`product/${product.slug}`}>
         <a>
           <img
             src={product.image}
             alt={product.name}
-            className="rounded shadow"
+            className='rounded shadow'
           />
         </a>
       </Link>
-      <div className="flex flex-col items-center justify-center p-5">
+      <div className='flex flex-col items-center justify-center p-5'>
         <Link href={`/products/${product.slug}`}>
           <a>
-            <h5 className="text-lg">{product.name}</h5>
+            <h5 className='text-lg'>{product.name}</h5>
           </a>
         </Link>
-        <p className="mb-2">{product.brand}</p>
+        <p className='mb-2'>{product.brand}</p>
         <p>₹{product.price}</p>
-        <button className="primary-button " type="button">Add to cart</button>
+        <button
+          className='primary-button '
+          type='button'
+          onClick={() => addToCartHandler(product)}
+        >
+          Add to cart
+        </button>
       </div>
     </div>
   );
--- pages/product/[slug].js
+++ pages/product/[slug].js
@@ -1,30 +1,32 @@
+import axios from "axios";
 import Image from "next/image";
 import Link from "next/link";
 import { useRouter } from "next/router";
 import React, { useContext } from "react";
+import { toast } from "react-toastify";
 import Layout from "../../components/Layout";
-import data from "../../utils/data";
+import Product from "../../models/Product";
+
+import db from "../../utils/db";
 import { Store } from "../../utils/Store";
 
-export default function ProductScreen() {
+export default function ProductScreen(props) {
+  const { product } = props;
   const { state, dispatch } = useContext(Store);
 
   const router = useRouter();
 
-  const { query } = useRouter();
-  const { slug } = query;
-  const product = data.products.find((x) => x.slug === slug);
   if (!product) {
-    return <div>Product Not Found</div>;
+    return <Layout title='Product Not Found'>Product Not Found</Layout>;
   }
 
-  const addToCartHandler = () => {
+  const addToCartHandler = async () => {
     const existItem = state.cart.cartItems.find((x) => x.slug === product.slug);
     const quantity = existItem ? existItem.quantity + 1 : 1;
+    const { data } = await axios.get(`/api/products/${product._id}`);
 
-    if (product.countInStock < quantity) {
-      alert("Sorry. Product is out of stock");
-      return;
+    if (data.countInStock < quantity) {
+      return toast.error("Sorry , product is out of stock");
     }
 
     dispatch({ type: "CART_ADD_ITEM", payload: { ...product, quantity } });
@@ -33,23 +35,23 @@ export default function ProductScreen() {
 
   return (
     <Layout title={product.name}>
-      <div className="py-2">
-        <Link href="/">back to products</Link>
+      <div className='py-2'>
+        <Link href='/'>back to products</Link>
       </div>
-      <div className="grid md:grid-cols-4 md:gap-3">
-        <div className="md:col-span-2">
+      <div className='grid md:grid-cols-4 md:gap-3'>
+        <div className='md:col-span-2'>
           <Image
             src={product.image}
             alt={product.name}
             width={640}
             height={640}
-            layout="responsive"
+            layout='responsive'
           ></Image>
         </div>
         <div>
           <ul>
             <li>
-              <h1 className="text-lg">{product.name}</h1>
+              <h1 className='text-lg'>{product.name}</h1>
             </li>
             <li>Category: {product.category}</li>
             <li>Brand: {product.brand}</li>
@@ -60,18 +62,18 @@ export default function ProductScreen() {
           </ul>
         </div>
         <div>
-          <div className="card p-5">
-            <div className="mb-2 flex justify-between">
+          <div className='card p-5'>
+            <div className='mb-2 flex justify-between'>
               <div>Price</div>
               <div>${product.price}</div>
             </div>
-            <div className="mb-2 flex justify-between">
+            <div className='mb-2 flex justify-between'>
               <div>Status</div>
               <div>{product.countInStock > 0 ? "In stock" : "Unavailable"}</div>
             </div>
             {/* <button className="primary-button w-full">Add to cart</button> */}
             <button
-              className="primary-button w-full"
+              className='primary-button w-full'
               onClick={addToCartHandler}
             >
               Add to cart
@@ -81,4 +83,17 @@ export default function ProductScreen() {
       </div>
     </Layout>
   );
+}
+
+export async function getServerSideProps(context) {
+  const { params } = context;
+  const { slug } = params;
+  await db.connect();
+  const product = await Product.findOne({ slug }).lean();
+  await db.disconnect();
+  return {
+    props: {
+      product: product ? db.convertDocToObj(product) : null,
+    },
+  };
 }
